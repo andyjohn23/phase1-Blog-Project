0887e6728aa513140be1aad3aadee42fffa53851
andyjohn23 <andrewnick906@gmail.com>
2022-09-15 08:27:29 +0300

    created the create blog code

---
+++ assets/js/createBlog.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('form');
+
+const createBlog = async (e) => {
+    e.preventDefault();
+
+    const blogBody = {
+        title: form.title.value,
+        body: form.body.value,
+        likes: 0
+    }
+
+    await fetch('http://localhost:3000/posts', {
+        method: 'POST',
+        body: JSON.stringify(blogBody),
+    })
+
+    window.location.replace('/');
+}
+
+
+form.addEventListener('submit', createBlog);
