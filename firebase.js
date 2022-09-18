6e883d7f63a89cbf284666bfe264a14356b9c43a
andyjohn23 <andrewnick906@gmail.com>
2022-09-18 14:16:55 +0300

    deployed to firebase

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC8r3myhpri7JulXFjpX2XOXL3GgBo4Lu8",
+    authDomain: "phase1-blog-andy.firebaseapp.com",
+    projectId: "phase1-blog-andy",
+    storageBucket: "phase1-blog-andy.appspot.com",
+    messagingSenderId: "68923659078",
+    appId: "1:68923659078:web:7cd3b81dca60bf4e768b47",
+    measurementId: "G-JQ8546NR78"
+};
---
+++ public/assets/js/blogDetail.js
@@ -0,0 +1,32 @@
+const id = new URLSearchParams(window.location.search).get('id');
+const containerBlog = document.querySelector('.read-modal');
+const deleteButton = document.querySelector('.delete-blog, .read-modal');
+
+const renderBlogDetail = async () => {
+    const response = await fetch("https://phase1-blog.herokuapp.com/posts/" + id);
+    const blog = await response.json();
+
+    const blogDetailTemplate =
+        `<div class="card">
+            <div class="card-body">
+                <h5 class="card-title">${blog.title}</h5>
+                <hr />
+                <p class="card-text">${blog.body}</p>
+                <div class="card-buttons">
+                    <a href="/"><i class="material-icons delete-blog" title="Back">arrow_back</i></a>
+                    <i class="material-icons delete-blog" title="Delete">delete</i>
+                </div>
+            </div>
+          </div>`;
+    containerBlog.innerHTML = blogDetailTemplate;
+
+    deleteButton.addEventListener('click', async (e) => {
+        const response = await fetch("https://phase1-blog.herokuapp.com/posts/" + id, {
+            method: "DELETE"
+        });
+        window.location.replace('/');
+    })
+}
+
+window.addEventListener('DOMContentLoaded', () => renderBlogDetail())
+
