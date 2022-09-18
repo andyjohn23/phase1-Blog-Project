29bac2a684351e5b99f434ee57ed49cb0ec4440f
andyjohn23 <andrewnick906@gmail.com>
2022-09-18 14:18:17 +0300

    deployed to firebase

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -16,7 +16,7 @@ const renderBlogs = async () => {
                 <div class="card-body">
                     <p class="card-text">${blog.body.slice(0, 150)}</p>
                     <div class="card-buttons">
-                        <a href="/blogDetail.html?id=${blog.id}/" class="btn btn-primary">Read more</a>
+                        <a href="/blogDetail.html?id=${blog.id}" class="btn btn-primary">Read more</a>
                         <div class="likeButton">
                             <i class="material-icons outlined">thumb_up</i>
                             <span>${blog.likes}</span>
