fbbe6951d4efa298fd71496603c2af5670f6ed49
andyjohn23 <andrewnick906@gmail.com>
2022-09-18 14:37:42 +0300

    remove delete blog class name in back button

---
+++ public/assets/js/createBlog.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('form');
+
+const createBlog = async (e) => {
+    e.preventDefault();
+
+    const blogBody = {
+        title: form.title.value,
+        body: form.body.value,
+        likes: 0
+    }
+
+    await fetch('http://localhost:3000/posts', {
+        method: 'POST',
+        body: JSON.stringify(blogBody),
+        headers: { 'Content-Type': 'application/json' }
+    })
+
+    // Swal.fire(
+    //     'Blog Posted Successfully!'
+    // )
+
+    window.location.replace('/');
+}
+
+
+form.addEventListener('submit', createBlog);
